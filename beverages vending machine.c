162c018295ec4a264af479dd6acd1a3600e88fa9
Vikas-sm-0017 <vikasvikki4803@gmail.com>
2024-03-24 18:34:14 +0530

    ver 1.0

---
+++ beverages vending machine.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main() {
+    printf("Welcome to the Beverage Vending Machine\n");
+    printf("\n");
+    printf("1. Coca-Cola\n");
+    printf("2. Pepsi\n");
+    printf("3. Sprite\n");
+    printf("4. Fanta\n");
+    printf("5. Mountain Dew\n");
+    printf("6. Water\n");
+    printf("7. Exit\n");
+    printf("\n");
+    printf("select an option (1-7): ");
+
+    return 0;
+}
