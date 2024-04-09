278bfd39a165fe6dbb6749b0ae44ec543a2747b9
Vikas-sm-0017 <vikasvikki4803@gmail.com>
2024-04-09 15:11:50 +0530

    ver 7.5

--- beverages vending machine.c
+++ beverages vending machine.c
@@ -8,128 +8,130 @@
 
 #include <stdio.h>
 
+void displayMenu()
+{
+    printf("\nBeverages Vending Machine\n");
+    printf("1. Coffee\n");
+    printf("2. Tea\n");
+    printf("3. Soda\n");
+    printf("4. Exit\n");
+    printf("Enter your choice: ");
+}
+
+void displaySizeOptions(const char *beverage, float smallPrice, float mediumPrice, float largePrice)
+{
+    printf("\n%s Sizes:\n", beverage);
+    printf("s. Small   - Rs %.2f\n", smallPrice);
+    printf("m. Medium  - Rs %.2f\n", mediumPrice);
+    printf("l. Large   - Rs %.2f\n", largePrice);
+    printf("Enter size choice (s/m/l): ");
+}
+
+void processChoice(int choice, float *price)
+{
+    char size_choice;
+    switch (choice)
+{
+        case 1:
+            displaySizeOptions("Coffee", 30.0, 40.0, 50.0);
+            scanf(" %c", &size_choice);
+            switch (size_choice)
+{
+                case 's':
+                    printf("You selected Small Coffee.\n");
+                    *price = 30.0;
+                    break;
+                case 'm':
+                    printf("You selected Medium Coffee.\n");
+                    *price = 40.0;
+                    break;
+                case 'l':
+                    printf("You selected Large Coffee.\n");
+                    *price = 50.0;
+                    break;
+                default:
+                    printf("Invalid size choice. Please enter 's', 'm', or 'l'.\n");
+                    *price = 0.0;
+}
+            break;
+        case 2:
+            displaySizeOptions("Tea", 25.0, 35.0, 45.0);
+            scanf(" %c", &size_choice);
+            switch (size_choice)
+{
+                case 's':
+                    printf("You selected Small Tea.\n");
+                    *price = 25.0;
+                    break;
+                case 'm':
+                    printf("You selected Medium Tea.\n");
+                    *price = 35.0;
+                    break;
+                case 'l':
+                    printf("You selected Large Tea.\n");
+                    *price = 45.0;
+                    break;
+                default:
+                    printf("Invalid size choice. Please enter 's', 'm', or 'l'.\n");
+                    *price = 0.0;
+}
+            break;
+        case 3:
+            displaySizeOptions("Soda", 20.0, 30.0, 40.0);
+            scanf(" %c", &size_choice);
+            switch (size_choice)
+{
+                case 's':
+                    printf("You selected Small Soda.\n");
+                    *price = 20.0;
+                    break;
+                case 'm':
+                    printf("You selected Medium Soda.\n");
+                    *price = 30.0;
+                    break;
+                case 'l':
+                    printf("You selected Large Soda.\n");
+                    *price = 40.0;
+                    break;
+                default:
+                    printf("Invalid size choice. Please enter 's', 'm', or 'l'.\n");
+                    *price = 0.0;
+}
+            break;
+        case 4:
+            printf("Exiting the vending machine.\n");
+            break;
+        default:
+            printf("Invalid choice. Please enter a number between 1 and 4.\n");
+            *price = 0.0;
+}
+}
+
 int main()
 {
     int choice;
-    char size_choice;
     char continue_choice;
     float price;
 
     do
 {
-        // Display menu
-        printf("\nBeverages Vending Machine\n");
-        printf("1. Coffee\n");
-        printf("2. Tea\n");
-        printf("3. Soda\n");
-        printf("4. Exit\n");
-        printf("Enter your choice: ");
+        displayMenu();
         scanf("%d", &choice);
+        processChoice(choice, &price);
 
-    // Process user choice
-    switch(choice)
+        // Display price if valid
+        if (price > 0.0)
 {
-    case 1:
-        printf("\nCoffee Sizes:\n");
-        printf("s. Small   - Rs 30\n");
-        printf("m. Medium  - Rs 40\n");
-        printf("l. Large   - Rs 50\n");
-        printf("Enter size choice (s/m/l): ");
-        scanf(" %c", &size_choice);
-    switch(size_choice)
-{
-    case 's':
-        printf("You selected Small Coffee.\n");
-        price = 30;
-    break;
-    case 'm':
-        printf("You selected Medium Coffee.\n");
-        price = 40;
-    break;
-    case 'l':
-        printf("You selected Large Coffee.\n");
-        price = 50;
-    break;
-    default:
-        printf("Invalid size choice. Please enter 's', 'm', or 'l'.\n");
-        price = 0.0;
-}
-    break;
-    case 2:
-        printf("\nTea Sizes:\n");
-        printf("s. Small   - Rs 25\n");
-        printf("m. Medium  - Rs 35\n");
-        printf("l. Large   - Rs 45\n");
-        printf("Enter size choice (s/m/l): ");
-        scanf(" %c", &size_choice);
-    switch(size_choice)
-{
-    case 's':
-        printf("You selected Small Tea.\n");
-        price = 25;
-    break;
-    case 'm':
-        printf("You selected Medium Tea.\n");
-        price = 35;
-    break;
-    case 'l':
-        printf("You selected Large Tea.\n");
-        price = 45;
-    break;
-    default:
-        printf("Invalid size choice. Please enter 's', 'm', or 'l'.\n");
-        price = 0.0;
-}
-    break;
-    case 3:
-        printf("\nSoda Sizes:\n");
-        printf("s. Small   - Rs 20\n");
-        printf("m. Medium  - Rs 30\n");
-        printf("l. Large   - Rs 40\n");
-        printf("Enter size choice (s/m/l): ");
-        scanf(" %c", &size_choice);
-    switch(size_choice)
-{
-    case 's':
-        printf("You selected Small Soda.\n");
-        price = 20;
-    break;
-    case 'm':
-        printf("You selected Medium Soda.\n");
-        price = 30;
-    break;
-    case 'l':
-        printf("You selected Large Soda.\n");
-        price = 40;
-    break;
-    default:
-        printf("Invalid size choice. Please enter 's', 'm', or 'l'.\n");
-        price = 0.0;
-}
-    break;
-    case 4:
-        printf("Exiting the vending machine.\n");
-    return 0;
-    default:
-        printf("Invalid choice. Please enter a number between 1 and 4.\n");
-        price = 0.0;
+            printf("Price: Rs%.2f\n", price);
 }
 
-    // Display price
-    if (price > 0.0)
-{
-        printf("Price: Rs%.2f\n", price);
-}
-
-    // Ask if user wants to continue
+        // Ask if user wants to continue
         printf("\nDo you want to continue? (y/n): ");
         scanf(" %c", &continue_choice);
 }
-    while(continue_choice == 'y' || continue_choice == 'Y');
+    while (continue_choice == 'y' || continue_choice == 'Y');
 
-        printf("Exiting the vending machine.\n");
+    printf("Exiting the vending machine.\n");
 
     return 0;
 }
-
-
